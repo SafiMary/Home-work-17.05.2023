0726b4f0432bcf29a924d513b3970649c441538a
Мария Сафи <safim@yandex.ru>
2023-05-28 18:05:41 +0500

    Add project files.

---
+++ Home work 17.05.2023/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work_17._05._2023
+{
+    
+    internal class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            string path1 = "text1.txt";
+            string path2 = "text2.txt";
+
+            try
+            {
+                path1 = args[0];
+                path2 = args[1];
+            }
+
+            catch
+            {
+                Console.WriteLine("аргументы не переданны");
+            }
+
+            var streamWriter = new StreamWriter("solution.txt");
+            string[] lines1 = File.ReadAllLines(path1);
+            Console.WriteLine(String.Join(Environment.NewLine, lines1));
+            string[] lines2 = File.ReadAllLines(path2);
+            Console.WriteLine(String.Join(Environment.NewLine, lines2));
+            //string[] lines1 = { "Пиво", "Вино", "Водка", "Вермут" };
+            //string[] lines2 = { "Пиво", "Сок", "Абсент", "Водка" };
+            var result = lines1.Intersect(lines2).ToList();
+            foreach (var s in result) { 
+                streamWriter.WriteLine(s);
+            }
+            streamWriter.Close();
+        }
+    }
+}
